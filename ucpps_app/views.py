082a3fafb2139d78d119f82d3cf6ce8f211f5bce
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-04-08 00:06:34 -0600

    Update views.py

--- ucpps_app/views.py
+++ ucpps_app/views.py
@@ -39,13 +39,14 @@ selectedFeatures = ['yearOfRegistration','powerPS','model','kilometer','monthOfR
 
 X = df[selectedFeatures]
 y = df['price']
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
+=
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 def name(request):    
     if request.method == "POST":
