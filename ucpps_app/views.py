7a4658a8fd59b1b9e9a0853d3322a05894c322b4
Tanu Nanda Prabhu <49044332+Tanu-N-Prabhu@users.noreply.github.com>
2020-04-08 00:14:50 -0600

    Update views.py

--- ucpps_app/views.py
+++ ucpps_app/views.py
@@ -39,7 +39,7 @@ selectedFeatures = ['yearOfRegistration','powerPS','model','kilometer','monthOfR
 
 X = df[selectedFeatures]
 y = df['price']
-=
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
